53c8e49806a3c28252b317266bf9e2b9ae2d085a
Hristiqn <hriso1997@abv.bg>
2015-12-03 00:17:03 +0200

    first point is almost ready

---
+++ DragRacing5/app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+	before_action :authenticate_user!
+	def show
+		@user=User.find(params[:id])
+	end	
+	def all
+		@members=Array.new
+		member=User.all
+		member.each do |mem|
+		@members<<mem.email	
+		end
+	end
+end
