2331115d7382b63c9fd4504d2bb33084ab8fe827
Hristiyan <hriso1997@gmail.com>
2016-01-21 18:42:11 +0200

    added date for an event

--- DragRacing5/db/schema.rb
+++ DragRacing5/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160111080252) do
+ActiveRecord::Schema.define(version: 20160121162232) do
 
   create_table "cars", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20160111080252) do
     t.string   "adress"
     t.integer  "user_id"
     t.integer  "comment_id"
+    t.datetime "date"
   end
 
   create_table "users", force: :cascade do |t|
