31dd4cb57d5e8468a2421c95e213ce26fa00e685
Hristiyan <hriso1997@gmail.com>
2016-01-22 22:03:00 +0200

    send email to event creator and give him info .Who will come to his event

--- DragRacing5/app/controllers/enrollments_controller.rb
+++ DragRacing5/app/controllers/enrollments_controller.rb
@@ -4,7 +4,6 @@ class EnrollmentsController < ApplicationController
 	end
 	def destroy
 		@enrollment=Enrollment.find(params[:enrollment_id])
-		UserMailer.welcome_email(current_user).deliver_now
 		@enrollment.delete
 		redirect_to root_url
 	end
@@ -12,7 +11,9 @@ class EnrollmentsController < ApplicationController
 		@event=Event.find(params[:id])
 		@enrollment = current_user.enrollments.build(enrollment_params)
 		@enrollment.save
-		UserMailer.welcome_email(current_user).deliver_now
+		event_creator_id=@event.user_id
+		event_creator=User.find(event_creator_id)
+		UserMailer.welcome_email(event_creator,current_user).deliver_now
 		@event.enrollments<<@enrollment
 		redirect_to root_url
 
--- DragRacing5/app/mailers/user_mailer.rb
+++ DragRacing5/app/mailers/user_mailer.rb
@@ -1,7 +1,8 @@
 class UserMailer < ApplicationMailer
 	default from: 'theschoolregister@gmail.com'
-	def welcome_email(user)
+	def welcome_email(user,user1)
     	@user = user
+    	@user1= user1
     	#@url  = 'http://localhost:3000/events/:id/enrollments/new'
     	mail(to: @user.email, subject: 'Welcome to My Awesome Site') 
   end
